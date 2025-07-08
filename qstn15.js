41dcc98142fcdd06bfcbfb2c340373bb534f2b14
Adarshjithu77 <adarshjithu77@gmail.com>
2025-07-08 16:39:42 +0530

    fiftteenthqstn done

---
+++ qstn15.js
@@ -0,0 +1,5 @@
+function checksequalornot(value1,value2){
+    return value1===value2 ? "same" : "different";
+}
+console.log(checksequalornot("abhi","abhi"));
+console.log(checksequalornot(7,"7"));
