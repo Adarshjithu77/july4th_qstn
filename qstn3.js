555ee98c0582ebbeebe7aeeaaf92f857b8715c1b
Adarshjithu77 <adarshjithu77@gmail.com>
2025-07-08 16:14:44 +0530

    thrdqstn done

---
+++ qstn3.js
@@ -0,0 +1,5 @@
+function adultorminor(string,age){
+    return result=age>18 ? string+"is an adult" : string+"is a minor";
+}
+console.log(adultorminor("Anandhu",22))
+console.log(adultorminor("Arun",17))
